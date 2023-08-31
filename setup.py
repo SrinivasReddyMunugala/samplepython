d5469ab95d99bf84471f291b1dded4b3b413632c
SrinivasReddyMunugala <SrinivasReddyMunugala@users.noreply.github.com>
2023-08-31 00:09:36 +0000

    ✅ Ready to clone and code.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for samplepython package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("samplepython", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="samplepython",
+    version=read("samplepython", "VERSION"),
+    description="Awesome samplepython created by SrinivasReddyMunugala",
+    url="https://github.com/SrinivasReddyMunugala/samplepython/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="SrinivasReddyMunugala",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["samplepython = samplepython.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
